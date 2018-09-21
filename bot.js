d1ee66a0f0025c1968a76384d16dbaa54bdc8dfb
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-09-21 09:40:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,13 @@ client.on("guildMemberAdd", member => {
 30Invite = 45k Credits ProBot
 وزيادة....
                                [ https://discord.gg/V7H8Zg6 ] 
+اما ذا السيرفر الاسطوري مدري اش اقولك والله والله
+لو مادخلت السيرفر ذا انت خسراان خالص
+10Invite = 1x FullAccess !!!!!!!!!! تخيل
+25Invite = 3x FullAcess
+50Invite = 5x FullAccess
+100Invite = 10x FullAcess + OptfineCape تخيل !!!!!!!!
+                               [ https://discord.gg/yQKbtNJ ]
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
